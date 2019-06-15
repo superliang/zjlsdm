c476ac9d91504b3d0a6394d33663ea6d65992e45
梁益宁 <zjlsdm@126.com>
2019-06-15 16:01:10 +0800

    Initial Commit

---
+++ pages/main-list/main-list.js
@@ -0,0 +1,95 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    list:[
+      {
+        id:"1",
+        company:"中央储备粮",
+        warehouse:"3P",
+        type:"早籼米",
+        turenum:"450",
+        totalnum:"700"
+      },{
+        id:"2",
+        company:"中央储备粮",
+        warehouse:"3P",
+        type:"早籼米",
+        turenum:"450",
+        totalnum:"700"
+      }, {
+        id: "3",
+        company: "中央储备粮",
+        warehouse: "3P",
+        type: "早籼米",
+        turenum: "450",
+        totalnum: "700"
+      }, {
+        id: "4",
+        company: "中央储备粮",
+        warehouse: "3P",
+        type: "早籼米",
+        turenum: "450",
+        totalnum: "700"
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+    
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+    
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+    
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+    
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+    
+  }
+})
