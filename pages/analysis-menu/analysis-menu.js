dcf495498972d1d043fc7ebaeff0b0f598f2ab66
梁益宁 <zjlsdm@126.com>
2019-06-16 14:37:10 +0800

    第一次提交代码
    稻谷运输系统，提交第一批基础逻辑页面

--- pages/analysis-menu/analysis-menu.js
+++ pages/analysis-menu/analysis-menu.js
@@ -1,52 +1,10 @@
 Page({
-  
+
   /**
    * 页面的初始数据
    */
   data: {
-    array: ["中储粮","市收储公司","衢江区收储公司"],
-    arraynums:[
-        ["1P","2P","3P"],
-        ["1S","2S","3S"],
-        ["1Q","2Q","3Q"]
-    ],
-    array3:["早籼谷","圆早籼谷","晚籼谷","粳谷"],
-    array4:["2019","2018","2017","2016","2015","2014"],
-    index: 0,
-    index1:0,
-    index3:0,
-    index4:0,
-    date: '2016-09-01',
-  },
-
-  onbindchange:function(e){
-    this.setData({
-      index:e.detail.value
-    })
-  },
-
-  bindpickerchange:function(e){
-    this.setData({
-      index1:e.detail.value
-    })
-  },
-
-  bindchange3:function(e){
-    this.setData({
-      index3:e.detail.value
-    })
-  },
-
-  bindchange4: function (e) {
-    this.setData({
-      index4: e.detail.value
-    })
-  },
-
-  bindDateChange:function(e){
-    this.setData({
-      date:e.detail.value
-    })
+    
   },
 
   /**
